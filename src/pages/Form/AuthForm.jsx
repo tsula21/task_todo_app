127f30d818024a3ddd3211d67d57264d7ce674eb
tsula <ika.tsulai21@gmail.com>
2023-04-28 14:48:09 +0400

    form container styling

---
+++ src/pages/Form/AuthForm.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+//
+import ApplicationTitle from "../../components/ApplicationTitle";
+import photo from "../../assets/add_photo.svg";
+
+const AuthForm = () => {
+  return (
+    <form className="w-[588px] h-[688px] bg-[#fff] mx-[auto]">
+      <div className="form_container"></div>
+    </form>
+  );
+};
+
+export default AuthForm;
