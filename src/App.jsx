286fde2a19519f86e6b9a41dc1e1d504c16349b0
tsula <ika.tsulai21@gmail.com>
2023-04-28 12:43:06 +0400

    A little folder structure, and installed React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Logo from "./assets/todo_logo.svg";
+import ApplicationLogo from "./components/ApplicationLogo";
+import Landing from "./pages/Landing/Landing";
 
 function App() {
-  return <div className=" text-[red]">Poppins</div>;
+  return (
+    <div className="App">
+      <div className="App_container">
+        <Landing />
+      </div>
+    </div>
+  );
 }
 
 export default App;
