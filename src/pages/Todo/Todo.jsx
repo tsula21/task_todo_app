363d42851c205824b0fa6acd7a7f8ad508e9f610
tsula <ika.tsulai21@gmail.com>
2023-04-29 23:06:25 +0400

    C:/Program Files/Git/

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import ApplicationTitle from "../../components/ApplicationTitle";
+import InputField from "./components/InputField";
 
 const Todo = () => {
   return (
     <div className="bg-[#292c33] min-h-screen">
       <Navbar />
+      <ApplicationTitle
+        className="text-[42px] text-[#000] font-[poppins-semi] text-center mt-[35px]"
+        Text="Add Your Daily Tasks"
+      />
+      <InputField />
     </div>
   );
 };
--- src/pages/Todo/components/Navbar.jsx
+++ src/pages/Todo/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between w-full px-[28px] py-[23px] bg-black">
-      <p className="text-[36px] font-[poppins-extraBold] cursor-pointer">
-        TO DO
-      </p>
+      <Link to="/">
+        <p className="text-[36px] font-[poppins-extraBold] cursor-pointer">
+          TO DO
+        </p>
+      </Link>
       <span className="flex items-center font-[poppins-light]">
         <h1 className="capitalize text-[#fff] text-[22px] ">your name</h1>
         <div className="bg-[#E6EBFF] w-[68px] h-[68px] rounded-full ml-[20px] cursor-pointer"></div>
