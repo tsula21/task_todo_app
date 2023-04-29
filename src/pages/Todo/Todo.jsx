2d42b5a70e8456485c6eaf885334ce8d3a124f53
tsula <ika.tsulai21@gmail.com>
2023-04-29 23:34:24 +0400

    todo input field markup

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import ApplicationTitle from "../../components/ApplicationTitle";
 import InputField from "./components/InputField";
+import SingleItem from "./components/SingleItem";
 
 const Todo = () => {
   return (
     <div className="bg-[#292c33] min-h-screen">
       <Navbar />
       <ApplicationTitle
-        className="text-[42px] text-[#000] font-[poppins-semi] text-center mt-[35px]"
+        className="text-[42px] text-[#000] font-[poppins-semi] text-center mt-[35px] mb-[27px]"
         Text="Add Your Daily Tasks"
       />
-      <InputField />
+      <section className="w-[595px] mx-[auto]">
+        <InputField />
+        <SingleItem />
+      </section>
     </div>
   );
 };
--- src/pages/Todo/components/InputField.jsx
+++ src/pages/Todo/components/InputField.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const InputField = () => {
-  return <div>InputField</div>;
+  return (
+    <div className="mb-[51px] flex h-[76px]">
+      <input
+        className="w-full rounded-s-[4px] pl-[24px] outline-none text-[#000] text-[22px] font-[poppins-light] bg-[#E6EBFF]"
+        type="text"
+        placeholder="my task"
+      />
+      <button className="bg-[#5EFC8D] rounded-e-[4px] active:bg-[#4a4a4a] w-[108px] text-[32px] text-black hover:bg-[black] hover:text-[#fff] transition-colors font-[poppins-light]">
+        Add
+      </button>
+    </div>
+  );
 };
 
 export default InputField;
