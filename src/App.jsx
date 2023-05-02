7a00e7d54ce0e950ced4c5fa4b5775b03a761cd9
tsula <ika.tsulai21@gmail.com>
2023-05-02 16:04:37 +0400

    Routing preoblems fixed, localstorage problems too

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,13 @@ import Todo from "./pages/Todo/Todo";
 const photoLocal = JSON.parse(localStorage.getItem("photo") || "[]");
 const nameLocal = JSON.parse(localStorage.getItem("name") || "[]");
 const nameTodos = JSON.parse(localStorage.getItem("todo") || "[]");
+const isUser = JSON.parse(localStorage.getItem("user") || false);
 
 function App() {
   const [photo, setPhoto] = useState(photoLocal);
   const [input, setInput] = useState("");
   const [todoList, setTodoList] = useState(nameTodos);
-  const [isRegistered, setIsRegistered] = useState(false);
+  const [isRegistered, setIsRegistered] = useState(isUser);
 
   const formik = useFormik({
     initialValues: {
@@ -65,11 +66,19 @@ function App() {
     setTodoList(todoList.filter((item) => item.id !== id));
   };
 
+  const clearUser = () => {
+    formik.setFieldValue("name", "");
+    setPhoto([]);
+    setIsRegistered(false);
+    setTodoList([]);
+  };
+
   useEffect(() => {
     localStorage.setItem("photo", JSON.stringify(photo));
     localStorage.setItem("name", JSON.stringify(formik.values.name));
     localStorage.setItem("todo", JSON.stringify(todoList));
-  }, [photo, formik.values.name, todoList]);
+    localStorage.setItem("user", JSON.stringify(isRegistered));
+  }, [photo, formik.values.name, todoList, isRegistered]);
   return (
     <BrowserRouter>
       <div className="App">
@@ -89,17 +98,30 @@ function App() {
               handleKeyPress,
               isRegistered,
               setIsRegistered,
+              clearUser,
             }}
           >
             <Routes>
               <Route exact path="/" element={<Landing />} />
+              <Route
+                path="*"
+                element={
+                  <div className="text-[40px] text-[green]">No found</div>
+                }
+              />
               {/* Redirect registered user */}
               {isRegistered ? (
                 <Route path="/AuthForm" element={<Navigate to="/Todo" />} />
               ) : (
                 <Route path="/AuthForm" element={<AuthForm />} />
               )}
-              <Route path="/Todo" element={<Todo />} />
+              {isRegistered ? (
+                <Route path="/Todo" element={<Todo />} />
+              ) : (
+                <Route path="/Todo" element={<Navigate to="/AuthForm" />} />
+              )}
+
+              {/* <Route path="/Todo" element={<Todo />} /> */}
             </Routes>
           </UserContext.Provider>
         </div>
--- src/components/ApplicationLogo.jsx
+++ src/components/ApplicationLogo.jsx
@@ -1,3 +1,3 @@
-export default function ApplicationLogo({ className, Logo, circle }) {
-  return <img className={className} src={Logo} ref={circle} alt="logo" />;
+export default function ApplicationLogo({ className, Logo, gsap }) {
+  return <img className={className} src={Logo} ref={gsap} alt="logo" />;
 }
